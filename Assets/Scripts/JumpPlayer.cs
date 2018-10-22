9437e4e46371f2ac125bc099152590f101db18ed
DefinitelyNotAShark <32074072+DefinitelyNotAShark@users.noreply.github.com>
2018-10-21 22:24:02 -0500

    Adding a test scene, applied move and jump scripts

---
+++ Assets/Scripts/JumpPlayer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private string jumpButtonName;
+
+    [SerializeField]
+    private LayerMask ground;//This is how we know what the player can jump on
+
+    [SerializeField]
+    private float jumpHeight;
+
+    [SerializeField]
+    private float normalGravityMultiplier;//gravity on the way up
+
+    [SerializeField]
+    private float fallMultiplier;//gravity on the way down
+
+
+    private Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    void Update ()
+    {
+        Debug.Log("On ground is: " + isOnGround().ToString());
+        Jump();
+    }
+
+    void Jump()
+    {
+        //if (rigidbody.velocity.y < 0)
+        //{
+        //    rigidbody.gravityScale = fallMultiplier;//this controls how fast the player falls
+        //}
+        //else
+        //{
+        //    rigidbody.gravityScale = normalGravityMultiplier;
+        //}
+
+        if (Input.GetButtonDown(jumpButtonName) && isOnGround())//if we on ground and we can jump
+        {
+            Debug.Log("trying to jump");
+            rigidbody.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+        }
+    }
+
+    private bool isOnGround()
+    {
+        if (Physics.Raycast(transform.position, Vector2.down, GetComponent<Renderer>().bounds.extents.y + .1f, ground))
+        {
+            return true;
+        }
+
+        else return false;
+    }
+}
---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField]
+    private string horizontalAxisName;
+
+    [SerializeField]
+    private string verticalAxisName;
+
+    [SerializeField]
+    private float speed;
+
+    private float horizontalAxisValue;
+    private float verticalAxisValue;
+
+    RaycastHit rayHit;
+    private bool canMove;
+    private Vector3 MoveVector;
+
+    void Update()
+    {
+        GetInput();
+    }
+
+    private void FixedUpdate()
+    {
+       // if (canMove)
+       // {
+            Move();        
+        //}
+    }
+
+    void GetInput()
+    {
+        horizontalAxisValue = Input.GetAxis(horizontalAxisName);//stores my input for each update
+        verticalAxisValue = Input.GetAxis(verticalAxisName);
+    }
+
+    private void Move()
+    {
+        MoveVector = new Vector3(horizontalAxisValue * speed * Time.deltaTime, 0, verticalAxisValue * speed * Time.deltaTime);
+        transform.Translate(MoveVector);
+    }
+}
