197f65d5f62fcbe0bf462da8628112ee167ddfa5
JWhittom <32077841+JWhittom@users.noreply.github.com>
2018-10-28 10:15:02 -0500

    Puzzle Prop mockup
    Adds basic puzzle prop mockups for ball, basket, and drawbridge.

---
+++ Assets/Scripts/Basket.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ball")
+            Debug.Log("Ball entered basket.");
+    }
+}
