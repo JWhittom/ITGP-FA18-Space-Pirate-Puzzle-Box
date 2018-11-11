3229d6b29eb2f9f2ba989adde422580293c4257f
JWhittom <32077841+JWhittom@users.noreply.github.com>
2018-11-11 17:56:58 -0600

    Started Work 11/11
    Created a scene and a pivot script, it doesn't work yet.

---
+++ Assets/Scripts/RoomPivot.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomPivot : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Button to use to pivot room.")]
+    private string pivotButtonName;
+
+    private bool shouldPivot;
+
+    private GameObject level;
+
+	// Use this for initialization
+	void Start ()
+    {
+        level = GameObject.FindGameObjectWithTag("Level");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GetPivotInput();
+	}
+
+    private void FixedUpdate()
+    {
+        if (shouldPivot)
+            PivotRoom();
+    }
+
+    private void PivotRoom()
+    {
+        level.transform.Rotate(Vector3.forward * -90, Space.World);
+    }
+
+    private void GetPivotInput()
+    {
+        shouldPivot = Input.GetButtonDown(pivotButtonName);
+    }
+}
